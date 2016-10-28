e77014e990e19410a6e85a407b6bd426e31bc666
KyleGacom <threeaday3@gmail.com>
2016-10-28 09:16:35 -0400

    Nav auto drive may have it working adjusting motors

--- student.py
+++ student.py
@@ -69,7 +69,7 @@ class GoPiggy(pigo.Pigo):
 
     def cruise(self):
         set_left_speed(100)
-        set_right_speed(115)
+        set_right_speed(110)
         print("is it clear in front?")
         clear = self.frontClear()
         print(clear)
