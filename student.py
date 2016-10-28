c9c239a3edd4ed45a3066a11820c3fee0ff55386
KyleGacom <threeaday3@gmail.com>
2016-10-28 09:07:19 -0400

    Nav auto drive added near clear method to check only the front and not the sides and set the speed slower ( trying to fix the frontClear)

--- student.py
+++ student.py
@@ -44,7 +44,7 @@ class GoPiggy(pigo.Pigo):
         menu.get(ans, [None, error])[1]()
 
     def frontClear(self) -> bool:
-        for x in range((self.MIDPOINT + 0), (self.MIDPOINT - 0)):
+        for x in range((self.MIDPOINT - 1), (self.MIDPOINT + 1), +0):
             servo(x)
             time.sleep(.1)
             scan1 = us_dist(15)
