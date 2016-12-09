b3af131129b967ce6ce048f9b3025bd87e730457
KyleGacom <threeaday3@gmail.com>
2016-12-09 09:40:44 -0500

    adjustmentsto motors

--- student.py
+++ student.py
@@ -15,7 +15,7 @@ class GoPiggy(pigo.Pigo):
     MIDPOINT = 96
     STOP_DIST = 20
     RIGHT_SPEED = 105
-    LEFT_SPEED = 107
+    LEFT_SPEED = 105
     TIME_PER_DEGREE = 0.00733333333
     turn_track = 0.0
 
